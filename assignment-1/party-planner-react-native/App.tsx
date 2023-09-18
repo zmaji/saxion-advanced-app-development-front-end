b70cce9816daacde61fb17d73bbe478828469768
Maurice <502241@student.saxion.nl>
2023-09-18 11:44:58 +0200

    party overview and individual party modal

--- assignment-1/party-planner-react-native/App.tsx
+++ assignment-1/party-planner-react-native/App.tsx
@@ -1,20 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity
+} from 'react-native';
 
-export default function App() {
+import { Party } from './src/types/Party';
+import MediaCard from './src/components/MediaCard/MediaCard';
+import PartyModal from './src/components/modal/PartyModal';
+
+const partyOne: Party = {
+  id: 1,
+  title: 'Party at Saxion',
+  description: 'description',
+  location: 'location 1',
+  datetime: new Date().toISOString(),
+};
+
+const partyTwo: Party = {
+  id: 2,
+  title: "Domino's",
+  description: 'description',
+  location: 'location 2',
+  datetime: new Date().toISOString(),
+};
+
+const partyThree: Party = {
+  id: 3,
+  title: 'Max his project X chess party',
+  description: 'description',
+  location: 'location 3',
+  datetime: new Date().toISOString(),
+};
+
+const partyArray: Party[] = [partyOne, partyTwo, partyThree];
+
+const PartyOverview: React.FC<{ parties: Party[] }> = () => {
+  const [selectedParty, setSelectedParty] = useState<Party | null>(null);
+
+  const openPartyModal = (party: Party) => {
+    setSelectedParty(party);
+  };
+
+  const closeModal = () => {
+    setSelectedParty(null);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* TODO: FIX HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Party overview</Text>
+      </View>
+
+      <Text style={styles.subheader}>All upcoming parties</Text>
+
+      <Text style={styles.context}>
+        Explore the details, mark your calendars, and prepare for an epic journey through the latest and greatest parties in town!
+      </Text>
+
+      <FlatList
+        data={partyArray}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => openPartyModal(item)}>
+            <View style={styles.partyContainer}>
+              <MediaCard party={item} />
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(party) => party.id.toString()}
+      />
+
+      {selectedParty && (
+        <PartyModal party={selectedParty} isVisible={true} closeModal={closeModal} />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  //TODO FIX HEADER
+  header: {
+    backgroundColor: 'ghostwhite',
+    padding: 10,
+    textAlign: 'center'
+  },
+  //TODO FIX HEADER
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  subheader: {
+    fontSize: 25,
+    margin: 10
+  },
+  context: {
+    margin: 25
+  },
+  partyContainer: {
+    padding: 10,
   },
 });
+
+export default PartyOverview;
---
+++ assignment-1/party-planner-react-native/src/components/modal/PartyModal.tsx
@@ -0,0 +1,85 @@
+import {
+  Modal,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+
+import { Party } from '../../types/Party';
+
+interface PartyModalProps {
+  party: Party;
+  isVisible: boolean;
+  closeModal: () => void;
+}
+
+const PartyModal: React.FC<PartyModalProps> = ({ party, isVisible, closeModal }) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={closeModal}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>{party.title}</Text>
+          <Text>{party.location}</Text>
+          <Text>{party.description}</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={closeModal}
+          >
+            <Text style={styles.buttonText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: '#2196F3',
+    marginTop: 15,
+    paddingHorizontal: 20
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+});
+
+export default PartyModal;
