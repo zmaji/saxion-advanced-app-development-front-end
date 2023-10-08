9c71c601f4c1af5ca2268ca80126e17b06a862d8
Nils Kimenai <493217@student.saxion.nl>
2023-10-08 15:02:22 +0200

    New routing structure with sidepanel to use in app with active items

--- assignment-2/fly-with-confidence/src/components/layout/AppHeader.tsx
+++ assignment-2/fly-with-confidence/src/components/layout/AppHeader.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {
+    View,
+    TouchableOpacity,
+    Text,
+    StyleSheet
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { getStatusBarHeight } from "react-native-status-bar-height";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { themeColors } from "../../styles/themeColors";
-import {fontFamilyStyles, typographyStyles} from "../../styles/typography";
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBars, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { themeColors } from '../../styles/themeColors';
+import { fontFamilyStyles, typographyStyles } from '../../styles/typography';
 
 const statusBarHeight = getStatusBarHeight();
 
-const AppHeader = () => {
+// @ts-ignore
+const AppHeader = ({ onSidePanelPress }) => {
     const navigation = useNavigation();
 
     const handleBackPress = () => {
         navigation.goBack();
     };
 
-    return (
-        <TouchableOpacity onPress={handleBackPress} style={styles.backHeader}>
-            <FontAwesomeIcon icon={faChevronLeft} color={themeColors.primary}/>
+    const openSidePanel = () => {
+        if (onSidePanelPress) {
+            onSidePanelPress();
+        }
+    };
 
-            <Text style={styles.backText}>Back</Text>
-        </TouchableOpacity>
+    return (
+        <View style={styles.headerContainer}>
+            <TouchableOpacity onPress={handleBackPress} style={styles.backButton}>
+                <FontAwesomeIcon icon={faChevronLeft} color={themeColors.primary} />
+                <Text style={styles.backText}>Back</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={openSidePanel} style={styles.sidePanelButton}>
+                <FontAwesomeIcon icon={faBars} color={themeColors.white} size={16} />
+            </TouchableOpacity>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    backHeader: {
+    headerContainer: {
         padding: 25,
         paddingTop: 10 + statusBarHeight,
         paddingBottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: themeColors.white
+    },
+    backButton: {
         flexDirection: 'row',
         alignItems: 'center',
     },
@@ -39,6 +61,11 @@ const styles = StyleSheet.create({
         ...typographyStyles.baseFontSize,
         ...fontFamilyStyles.loraBoldItalic
     },
+    sidePanelButton: {
+        backgroundColor: themeColors.primary,
+        padding: 15,
+        borderRadius: 100
+    }
 });
 
 export default AppHeader;
