4174bd39aecc7f3c8e1dffee3c8548c821c8af5c
Maurice <502241@student.saxion.nl>
2023-10-11 15:43:13 +0200

    PR feedback - category optional

--- assignment-2/fly-with-confidence/src/controllers/ArticleController.tsx
+++ assignment-2/fly-with-confidence/src/controllers/ArticleController.tsx
@@ -3,9 +3,9 @@ import type { Article } from '../typings/Article';
 import axios from 'axios';
 import { BASE_URL } from '../../config';
 
-const getArticles = async (category: string): Promise<Article[]> => {
+const getArticles = async (category?: string): Promise<Article[]> => {
   try {
-    const response = await axios.get(`${BASE_URL}/articles?category=${category}`);
+    const response = category ? await axios.get(`${BASE_URL}/articles?category=${category}`) : await axios.get(`${BASE_URL}/articles`);
     return response.data
   } catch (error) {
     throw error;
