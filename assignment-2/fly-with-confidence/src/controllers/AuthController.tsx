0092bfd41bb2d060346d26dde7e162a7a64df1f6
Nils Kimenai <493217@student.saxion.nl>
2023-10-16 14:37:21 +0200

    cleanup

--- assignment-2/fly-with-confidence/src/controllers/AuthController.tsx
+++ assignment-2/fly-with-confidence/src/controllers/AuthController.tsx
@@ -1,18 +1,12 @@
 import type { UserCredentials } from '../typings/User';
 
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BASE_URL } from '../../config';
 
 const loginUser = async (user: UserCredentials): Promise<string | null> => {
-  console.log('entering login function');
   try {
-    console.log('sending request with credentials:', user);
     const response = await axios.post(`${BASE_URL}/credentials`, user);
-    if (response) {
-      await AsyncStorage.setItem('token', JSON.stringify(response.data));
-    }
-    console.log('response', response.data);
+
     return response.data
   } catch (error) {
     throw error;
