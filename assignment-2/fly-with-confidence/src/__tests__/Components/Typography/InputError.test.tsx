13dc2187c3e17251981a003cbc1b0913f56d89ad
zmaji <502241@student.saxion.nl>
2023-10-31 12:44:42 +0100

    inputerror component test

---
+++ assignment-2/fly-with-confidence/src/__tests__/Components/Typography/InputError.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import InputError from '../../../components/error/InputError';
+
+describe('InputError', () => {
+  it('should render the error message with the provided content, color, and custom styles', () => {
+    const errorMessage = 'This is an error message';
+    const color = 'error';
+    const customStyles = { fontSize: 16, fontWeight: 'bold' };
+
+    const { getByText } = render(
+        <InputError content={errorMessage} color={color} customStyles={customStyles} />,
+    );
+
+    const errorElement = getByText(errorMessage);
+    expect(errorElement).toBeTruthy();
+
+    const errorStyle = errorElement.props.style;
+    expect(errorStyle[1].color).toBe('#CC3333');
+    expect(errorStyle[2].fontSize).toBe(16);
+    expect(errorStyle[2].fontWeight).toBe('bold');
+  });
+
+  it('should render the error message with default styles', () => {
+    const errorMessage = 'This is an error message';
+
+    const { getByText } = render(
+        <InputError content={errorMessage} />,
+    );
+
+    const errorElement = getByText(errorMessage);
+    expect(errorElement).toBeTruthy();
+
+    const errorStyle = errorElement.props.style;
+    console.log(errorStyle);
+    expect(errorStyle[1].color).toBe('#333333');
+  });
+});
