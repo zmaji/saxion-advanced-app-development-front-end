3b5ea783fb890280fee2a4d8a58adc97d1626f42
Nils Kimenai <493217@student.saxion.nl>
2023-10-31 13:52:01 +0100

    test configuration expansion and code coverage

--- assignment-2/fly-with-confidence/jest.config.js
+++ assignment-2/fly-with-confidence/jest.config.js
@@ -3,4 +3,16 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  collectCoverage: process.env.npm_lifecycle_event === 'test:coverage',
+  collectCoverageFrom: ['src/**/*.tsx'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/src/typings',
+  ],
 };
