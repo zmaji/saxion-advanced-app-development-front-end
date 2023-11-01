376e1d2b8c51738deb94b1795a9df6aa1bd9df73
Maurice <502241@student.saxion.nl>
2023-11-01 10:20:50 +0100

    appheader test

--- assignment-2/fly-with-confidence/src/__tests__/Components/Layouts/AppHeader.test.tsx
+++ assignment-2/fly-with-confidence/src/__tests__/Components/Layouts/AppHeader.test.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-import AppHeader from '../../../components/layout/AppHeader'
+import { NavigationContainer } from '@react-navigation/native';
+import AppHeader from '../../../components/layout/AppHeader';
 
 describe('AppHeader', () => {
-  it('should render the header with a back button and side panel button', () => {
-    const mockBackPress = jest.fn();
+  it('should render the header with a side panel button', () => {
     const mockSidePanelPress = jest.fn();
 
-    const { getByText, getByTestId } = render(
-      <AppHeader onSidePanelPress={mockSidePanelPress} />
+    const { getByTestId } = render(
+      <NavigationContainer>
+        <AppHeader onSidePanelPress={mockSidePanelPress} />
+      </NavigationContainer>
     );
 
-    const backButton = getByText('Back');
     const sidePanelButton = getByTestId('side-panel-button');
 
-    expect(backButton).toBeTruthy();
     expect(sidePanelButton).toBeTruthy();
 
-    fireEvent.press(backButton);
     fireEvent.press(sidePanelButton);
-
-    expect(mockBackPress).toHaveBeenCalled();
     expect(mockSidePanelPress).toHaveBeenCalled();
   });
 });
