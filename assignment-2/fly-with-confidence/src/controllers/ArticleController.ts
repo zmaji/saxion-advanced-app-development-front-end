f84b583ecd848a0de26240765102051a9d05e683
Nils Kimenai <493217@student.saxion.nl>
2023-11-01 13:55:32 +0100

    Offline functionality of articles, save articles in SQLite db to use when offline

--- assignment-2/fly-with-confidence/src/controllers/ArticleController.ts
+++ assignment-2/fly-with-confidence/src/controllers/ArticleController.ts
@@ -2,12 +2,25 @@ import type { Article } from '../typings/Article';
 
 import axios from 'axios';
 import { BASE_URL } from '../../config';
+import ArticleModel from '../model/ArticleModel';
+import { isOnline } from '../utils/NetworkDetection';
 
 const getArticles = async (category?: string): Promise<Article[]> => {
   try {
-    const response = await axios.get(`${BASE_URL}/articles${category ? `?category=${category}` : ''}`);
+    if (await isOnline()) {
+      const response = await axios.get(`${BASE_URL}/articles${category ? `?category=${category}` : ''}`);
+      const articles = response.data;
 
-    return response.data;
+      await ArticleModel.createTable();
+
+      for (const article of articles) {
+        await ArticleModel.insertArticle(article);
+      }
+
+      return articles;
+    } else {
+      return await ArticleModel.getArticles(category);
+    }
   } catch (error) {
     throw error;
   }
@@ -15,9 +28,15 @@ const getArticles = async (category?: string): Promise<Article[]> => {
 
 const getArticle = async (articleID: string): Promise<Article | undefined> => {
   try {
-    const response = await axios.get(`${BASE_URL}/articles/${articleID}`);
+    if (await isOnline()) {
+      const response = await axios.get(`${BASE_URL}/articles/${articleID}`);
 
-    return response.data;
+      return response.data;
+    } else {
+      const article: Article| null = await ArticleModel.getArticle(articleID);
+
+      return article ? article : undefined;
+    }
   } catch (error) {
     throw error;
   }
