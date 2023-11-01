3114dbd0f4243b170f84b58b88fa54242eda5400
Maurice <502241@student.saxion.nl>
2023-11-01 10:31:59 +0100

    sidepanelitem test

---
+++ assignment-2/fly-with-confidence/src/__tests__/Components/Layouts/SidePanelitem.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import SidePanelItem from '../../../components/layout/SidePanelItem';
+
+describe('SidePanelItem', () => {
+  it('should render a side panel item with the provided title and icon', () => {
+    const title = 'Item Title';
+    const icon = 'newspaper';
+    const onPress = jest.fn();
+
+    const { getByText, getByTestId } = render(
+      <SidePanelItem title={title} icon={icon} onPress={onPress} />
+    );
+
+    const titleElement = getByText(title);
+    const itemIconContainer = getByTestId('sidepanel-item-icon-container');
+    const containerChildren = itemIconContainer.children;
+    const iconElement = containerChildren[0];
+    const iconName = iconElement.props.icon.iconName;
+
+    expect(titleElement).toBeTruthy();
+    expect(iconElement).toBeTruthy();
+
+    expect(titleElement.props.children).toContain(title);
+    expect(iconName).toBe('newspaper');
+  });
+
+  it('should call the onPress function when pressed', () => {
+    const title = 'Item Title';
+    const icon = 'newspaper';
+    const onPress = jest.fn();
+
+    const { getByText, getByTestId } = render(
+      <SidePanelItem title={title} icon={icon} onPress={onPress} />
+    );
+
+    const titleElement = getByText(title);
+    fireEvent.press(titleElement);
+
+    expect(onPress).toHaveBeenCalled();
+  });
+});
